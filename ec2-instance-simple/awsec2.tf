f4f54f1528609b5fb5f7832cf5584bf67814c44b
Alex Bleasdale <ableasdale@confluent.io>
2023-01-14 15:26:46 +0000

    Simple example - create a t2.micro instance and specify a keypair to allow ssh accesss

--- ec2-instance-simple/awsec2.tf
+++ ec2-instance-simple/awsec2.tf
@@ -5,9 +5,40 @@ provider "aws" {
 resource "aws_instance" "ubuntu_instance" {
     ami = "ami-01b8d743224353ffe" # Ubuntu 22.04 LTS (64bit x86)
     instance_type = "t2.micro"
+    key_name = "*** NAME OF YOUR KEY PAIR GOES HERE ***"
+    vpc_security_group_ids = [aws_security_group.main.id]
 
     tags = {
         Name = "ableasdale-ubuntu-instance"
         "Terraform" = "Yes"
     }
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
 }
