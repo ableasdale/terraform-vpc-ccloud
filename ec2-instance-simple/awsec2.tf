b09d41fb2cb98b00195fa856848f78bf26a2e7dd
Alex Bleasdale <ableasdale@confluent.io>
2023-01-17 13:44:29 +0000

    Added IP6 routing to security group

--- ec2-instance-simple/awsec2.tf
+++ ec2-instance-simple/awsec2.tf
@@ -20,7 +20,7 @@ resource "aws_security_group" "main" {
       cidr_blocks      = [ "0.0.0.0/0", ]
       description      = ""
       from_port        = 0
-      ipv6_cidr_blocks = []
+      ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = []
       protocol         = "-1"
       security_groups  = []
@@ -33,7 +33,7 @@ resource "aws_security_group" "main" {
      cidr_blocks      = [ "0.0.0.0/0", ]
      description      = ""
      from_port        = 22
-     ipv6_cidr_blocks = []
+     ipv6_cidr_blocks = ["::/0"]
      prefix_list_ids  = []
      protocol         = "tcp"
      security_groups  = []
